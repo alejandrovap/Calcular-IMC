448787396a3e7966188bdbcb68d9aab1cc9356ae
Alejandro <a23alejandrovp@iessanclemente.net>
2025-11-18 13:27:56 +0100

    second commit

--- app/src/main/java/com/example/calcular_imc/ResultImcFragment.kt
+++ app/src/main/java/com/example/calcular_imc/ResultImcFragment.kt
@@ -1,18 +1,17 @@
 package com.example.calcular_imc
 
-import android.app.Fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class ImcCalculatorFragment : Fragment() {
+class ResultImcFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imc_calculator, container, false)
+        return inflater.inflate(R.layout.fragment_result_imc, container, false)
     }
 }
